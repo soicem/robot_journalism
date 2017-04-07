80aacbed98f4bf572d88baaa64d3ff4d98a9d866
kim namgue <knq1130@naver.com>
2017-04-07 15:39:42 +0900

    mysql table query append

--- mysqlDB.py
+++ mysqlDB.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python -W ignore::DeprecationWarning
 
+# - MySQL robotjournarism database table query
+# use robotjournalism;
+#
+# DROP TABLE IF EXISTS articles;
+#
+# create table articles (
+# 	_id int not null auto_increment primary key,
+#     tendency int not null,
+#     target varchar(30) not null,
+#     article TEXT,
+#     articleUrl varchar(50) not null,
+#     publish_time datetime,
+#     collecting_time datetime
+# );
+#
+# insert into articles values(null , 1, "한겨례", "best in the world", "knq1130@naver.com",
+# "9999-12-31 23:59:59", "9999-12-31 23:59:59");
+# select * from articles;
+
+
 import pymysql as mq
 import time
 
