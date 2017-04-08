4ae2169cc68c16ee16abfc949d74aa9005838c43
kim namgue <knq1130@naver.com>
2017-04-08 17:12:54 +0900

    MySQL 연동

--- Article.py
+++ Article.py
@@ -4,9 +4,13 @@ import datetime
 import random
 import re
 
+import mysqlDB
+
+
 class Article():
-    def __init__(self, status):
-        self.status = status
+    def __init__(self, tendency):
+        self.mq = mysqlDB.mysqlDB()
+        self.tendency = tendency
         self.progressive = ["한겨례", "경향신문", "오마이뉴스"]
         self.progressivismTargets = {
             self.progressive[0]: "http://www.hani.co.kr/",
@@ -25,25 +29,43 @@ class Article():
 
     def gathering(self):
         def getLinks(articleUrl, i):
-            html = urlopen(self.progressivismTargets[self.progressive[i]] + articleUrl)
+            url = self.progressivismTargets[self.progressive[i]] + articleUrl
+            html = urlopen(url)
             bsObj = BeautifulSoup(html, "html.parser")
 
-            if i == 0:
+            if i == 0: # 0 : 한겨례
                 nameList = bsObj.findAll("div", {"class": "text"})
-                for name in nameList:
-                    print(name.get_text())
+                titles = bsObj.findAll("span", {"class": "title"})
+                published_times = bsObj.find_all('meta', attrs={'property':'article:published_time'})
+                article_result = ""
+                title = ""
+                for buf in titles:
+                    # print(title.get_text())
+                    title = buf.get_text()
+                p_time = ""
+                for published_time in published_times:
+                    p_date = re.compile(r'(\d{1,4}[/-]\d{1,2}[/-]\d{1,2}[T]\d{1,2}[/:]\d{1,2}[/:]\d{1,2}[+]\d{1,2}[/-:]\d{1,2})')
+                    p_time = p_date.findall(str(published_time))
+
+                for name in nameList:  article_result += name.getText().strip().replace("\n", "")
+                if (title is not ""):
+                    p_time = (p_time[0])[0:10] + " " + (p_time[0])[11:19]
+                    self.mq.insert_data(str(1), title, "한겨례", article_result, url, p_time)
+
                 return bsObj.find().findAll("a", href=re.compile("^(/arti/politics/)((?!:).)*$"))
             else:
                 pass
 
-        if self.status == "progressivism":
+        if self.tendency == "progressivism":
             links = getLinks(self.progressiveArticle[self.progressive[0]], 0)
-            while len(links) > 0:
+            for i in range(10):
                 newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
                 print(newArticle)
                 links = getLinks(newArticle, 0)
         elif self.status == "conservatism":
             pass
+
+
 
 
 
--- mysqlDB.py
+++ mysqlDB.py
@@ -8,6 +8,7 @@
 # create table articles (
 # 	_id int not null auto_increment primary key,
 #     tendency int not null,
+#     title varchar(30),
 #     target varchar(30) not null,
 #     article TEXT,
 #     articleUrl varchar(50) not null,
@@ -15,21 +16,50 @@
 #     collecting_time datetime
 # );
 #
-# insert into articles values(null , 1, "한겨례", "best in the world", "knq1130@naver.com",
-# "9999-12-31 23:59:59", "9999-12-31 23:59:59");
+# insert into articles values(null , 1, "쿵짝쿵짝 도천 천곡", "한겨례", "best in the world", "knq1130@naver.com","9999-12-31 23:59:59", "9999-12-31 23:59:59");
 # select * from articles;
 
 import pymysql
+import time
 
-# MySQL Connection 연결
-conn = pymysql.connect(host='localhost', user='root', password='1234',
-                       db='robotjournalism', charset='utf8')
+class mysqlDB:
+    def __init__(self):
+        self.conn = None
+        self.curs = None
+        self.mysql_init()
 
-# Connection 으로부터 Cursor 생성
-curs = conn.cursor()
+    def mysql_init(self):
+        # MySQL Connection 연결
+        self.conn = pymysql.connect(host='localhost', user='root', password='1234',
+                               db='robotjournalism', charset='utf8')
+        # Connection 으로부터 Cursor 생성
+        self.curs = self.conn.cursor()
 
-sql = "select * from articles"
-curs.execute(sql)
+    def mysql_close(self):
+        self.curs.close()
+        self.conn.close()
 
-rows = curs.fetchall()
-print(rows)  # 전체 rows
+    def insert_data(self, tendency, title, target, article, articleUrl, published_time):
+        now = time.localtime()
+        s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+        print(title)
+        sql = "insert into articles values(null , "+ \
+              tendency +',' \
+              '"' + title + '",' + \
+              '"' + target + '",' + \
+              '"' + article +'",' + \
+              '"' + articleUrl +'",' + \
+              '"' + published_time + '",' + \
+              '"' + s + '");'
+        print(sql)
+        self.curs.execute(sql)
+        self.conn.commit()
+    def printData(self):
+        sql = "select * from articles"
+        self.curs.execute(sql)
+        rows = self.curs.fetchall()
+        for row in rows:
+            print(row)
+
+
+
