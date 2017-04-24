ea3a365bb112428629773255615f5e8e6ce3ecfc
seungahhan <tmddk2736@naver.com>
2017-04-24 17:00:24 +0900

    오마이뉴스 및 필터링기능 추가
    .

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import Article
+from robot_journalism import Article
 
 if __name__=="__main__":
-    # tendency = input("tendency : ")
-    # keyword = input("keyword : ")
-    keyword='안철수'
-    ar = Article.Article("progressivism", keyword)
-    # ar = Article.Article(tendency, keyword)
+    tendency = input("tendency : ")
+    keyword = input("keyword : ")
+    ar = Article.Article(tendency, keyword)
     ar.gathering()
