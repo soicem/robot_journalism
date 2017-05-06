ab6c86152fa777011333fba2f8c14f075bd92be5
seungahhan <tmddk2736@naver.com>
2017-05-06 20:59:30 +0900

    비동기추가
    비동기추가

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from robot_journalism import Article
+import Article
+import asyncio
+
+from example_code.clockdeco_param import clock
+
+@clock('{name}({args}) dt={elapsed:0.3f}s')
+def main(tendency,keyword):
+    ar2 = Article.Article(tendency, keyword)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ar2.gathering(loop))
+
 
 if __name__=="__main__":
     tendency = input("tendency : ")
     keyword = input("keyword : ")
-    ar = Article.Article(tendency, keyword)
-    ar.gathering()
+    main(tendency,keyword)
+
+
