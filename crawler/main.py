243310c39ba5c13ba1084ebf55336fc188968b88
kim namgue <knq1130@naver.com>
2017-05-10 15:40:31 +0900

    My_Test 개선, crawler & 웹페이지 연동

--- crawler/main.py
+++ crawler/main.py
@@ -1,5 +1,6 @@
-from crawler.Article import *
+import Article
 import asyncio
+import socket
 
 from example_code.clockdeco_param import clock
 
@@ -9,10 +10,42 @@ def main(tendency,keyword):
     loop = asyncio.get_event_loop()
     loop.run_until_complete(ar2.gathering(loop))
 
+def getTendency_Keyword():
+    # create a socket object
+    print("waiting client connection...")
+    serversocket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+
+    # get local machine name
+    host = socket.gethostname()
+
+    port = 8090
+
+    # bind to the port
+    serversocket.bind((host, port))
+
+    # queue up to 5 requests
+    serversocket.listen(5)
+
+
+    # establish a connection
+    clientsocket, addr = serversocket.accept()
+    msg = clientsocket.recv(1024)
+    print("soicem")
+    #msg = str(msg.decode('unicode'), 'utf-8')
+    result = msg.decode('ascii').split(',')
+    # result = msg.split(',')
+    print("Got a connection from %s" % str(addr))
+
+    clientsocket.close()
+    return (result[0],result[1])
 
 if __name__=="__main__":
-    tendency = input("tendency : ")
-    keyword = input("keyword : ")
-    main(tendency,keyword)
+    result = getTendency_Keyword()
+    keyword, tendency = result
+    print(result)
+    # tendency = input("tendency : ")
+    # keyword = input("keyword : ")
+    # main(tendency,keyword)
 
 
